37be659e33e9d47642ed83d1ac815639fe3a4c0b
megan <mnbarber1993@gmail.com>
2025-03-22 13:33:33 -0700

    trying again

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/auth": "https://mothership-backend-b502f96270d5.herokuapp.com"
+    }
+  },
   plugins: [react()],
 })
